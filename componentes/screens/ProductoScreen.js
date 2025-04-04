25430f15ce5303aae0e0e69f39ba74c7341fe925
Jack625a <ktfut15@gmail.com>
2025-04-04 15:55:55 -0400

    Airtable

--- componentes/screens/ProductoScreen.js
+++ componentes/screens/ProductoScreen.js
@@ -1,10 +1,14 @@
 //Importacion de dependencias
-import React from "react";
+import React, {useState,useEffect} from "react";
 //Componentes (Flatlist, Image)
 import { View,Text,FlatList,Image,StyleSheet, Button } from "react-native";
+import axios from "axios";
 
+const token= "patzox8huKGcf2Ttp.5a95cd3b9862d7087099ce2d4a63717bd1d53b9c59eda2fd6dbfb8453ab66c2e"
+const idBaseDatos= "appZ01xbGIuDfPpDv";
+const nombreTabla="Productos";
 //crear los datos locales
-const datos=[
+/*const datos=[
     {
         id:"1",
         nombre:"Producto1",
@@ -42,13 +46,16 @@ const datos=[
         imagen:'https://www.tiendaamiga.com.bo/media/catalog/product/cache/deb88dadd509903c96aaa309d3e790dc/d/i/dise_o_sin_t_tulo_10__3.png'
     },
 
-];
+];*/
+
+
 
 //Paso 2. COMPONENTE DEL CARDVIEW - plantilla
 const ProductoCard=({
     nombre,precio,imagen
 })=>{
-    <View style={styles.card}>
+return(
+<View style={styles.card}>
         <Image
             source={{uri:imagen}}
             style={styles.imagen}
@@ -63,15 +70,59 @@ const ProductoCard=({
         </Text>
 
     </View>
+
+
+);
+    
 }
 
 //Componente principal
 
 export default function ProductoScreen(){
+    //Variables para obtencion de datos
+    const [productos,setProductos]=useState([]);
+    const [cargar,setCargar]=useState(true);
+
+    //Funcion para obtner los datos de airtable
+    const obtenerProductos=async ()=>{
+        try {
+            const response=await axios.get(
+            `https://api.airtable.com/v0/${idBaseDatos}/${nombreTabla}`,
+            {
+                headers:{
+                    Authorization: `Bearer ${token}`
+                },
+            }
+            );
+            //Obtener los datos de la respuesta que encvia la base de datos
+            const productoData=response.data.records.map((registro)=>({
+                id:registro.id,
+                nombre:registro.fields.nombre,
+                precio:registro.fields.precio,
+                imagen:registro.fields.imagen,
+            }));
+            setProductos(productoData)
+        }catch(error){
+            console.error("Error al obtener los datos ",error)
+        }finally{
+            setCargar(false);
+        }
+    };
+
+    //Efecto
+    useEffect(()=>{
+        obtenerProductos();
+    },[]);
+
+
+
+
+
+
     return(
         <View style={styles.contenedor}>
             <FlatList
-                data={datos}
+                data={productos}
                 keyExtractor={(item)=>item.id}
                 renderItem={({item})=>
                     <ProductoCard 
