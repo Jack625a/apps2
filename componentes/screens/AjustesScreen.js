20d62319ef3f51feb184f10141802984d5579f58
Jack625a <ktfut15@gmail.com>
2025-04-11 16:30:02 -0400

    envio de datos

--- componentes/screens/AjustesScreen.js
+++ componentes/screens/AjustesScreen.js
@@ -1,11 +1,86 @@
 //Importacion de dependencias
-import React from "react";
-import { View,Text } from "react-native";
+import React, {useState} from "react";
+import { View,Text,TextInput,Button,StyleSheet,Alert } from "react-native";
+//Solicitud al servidor 
+import axios from "axios"
+
+//Paso 1. Configurar Airtable
+const apiKey="";
+const idbasedatos=""
+const nombreTabla="Productos"
+
 
 export default function AjustesScreen(){
+    const [nombre,setNombre]=useState("")
+    const [precio,setPrecio]=useState("")
+    const [imagen,setImagen]=useState("")
+
+    //Paso 2. Crear la funcion para enviarProductos
+    const agregarProducto= async()=>{
+        if(!nombre||!precio||!imagen){
+            Alert.alert("Error","Debe completar todos los datos...");
+            return;
+        }
+        try {
+            const response=await axios.post(
+            `https://api.airtable.com/v0/${idbasedatos}/${nombreTabla}`,
+            {
+                fields:{
+                    nombre:nombre,
+                    precio:precio,
+                    imagen:imagen,
+                },
+            },
+            {
+              headers:{
+                Authorization: `Bearer ${apiKey}`, 
+                "Content-Type":"application/json",
+              },
+            //ver la respueta
+           
+            }
+            );
+            console.log(response)
+            Alert.prompt("Producto Agregado","Se agrego correctamente el producto",response.data)
+            setNombre("");
+            setPrecio(""),
+            setImagen("");
+        }catch(error){
+            console.error("Error","No se pudo agregar los productos..",error);
+            Alert.alert("Error","No se pudo agregar el producto...")
+        }
+    };
+
     return(
         <View>
-            <Text>Pantalla ajustes</Text>
+            <Text style={styles.titulos}>Registro de Productos</Text>
+                <TextInput
+                    placeholder="Ingrese el nombre del producto."
+                    value={nombre}
+                    onChange={setNombre}
+                />
+                <TextInput
+                    placeholder="Ingrese el precio"
+                    value={precio}
+                    onChange={setPrecio}
+                    keyboardType="numeric"
+                />
+                <TextInput
+                    placeholder="Ingrese la url de la imagen"
+                    value={imagen}
+                    onChange={setImagen}
+                />
+                <Button
+                    title="Agregar Producto"
+                    onPress={agregarProducto}
+                />
         </View>
-    )
+    );
 }
+
+//Estilos
+const styles=StyleSheet.create({
+    titulos:{
+        fontSize:22
+    }
+})
